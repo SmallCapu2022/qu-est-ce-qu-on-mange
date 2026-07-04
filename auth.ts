9d13818f7baa75d81b384871f27813bd9a6027c7
Capucine <debailleulcd@gmail.com>
2026-07-04 23:59:26 +0200

    correction googleauthent prod

--- auth.ts
+++ auth.ts
@@ -1,9 +1,8 @@
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
 import { PrismaAdapter } from '@auth/prisma-adapter'
-import { PrismaClient } from '@/app/generated/prisma/client'
 
-const prisma = new PrismaClient()
+import { prisma } from '@/lib/prisma'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
